41c6620e32add35255c4ab5b9b7da59c5feab6b4
yjh270877 <81067127+yjh270877@users.noreply.github.com>
2021-09-27 17:35:14 +0900

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,7 +1,7 @@
 let runPy = new Promise(function(success, nosuccess) {
 
-  const { spawn } = require('child_process');
-  const pyprog = spawn('node', ['./ffmpg_u.js']);
+  const { spawn } = require('child_process');       //자식프로세스를 지정하여 실행
+  const pyprog = spawn('node', ['./ffmpg_u.js']);   //자식프로세스로 지정
 
   pyprog.stdout.on('data', function(data) {
 
@@ -14,15 +14,15 @@ let runPy = new Promise(function(success, nosuccess) {
   });
 });
 const fs=require('fs');
-const AWS = require('aws-sdk');
-const id = 'AKIAXU3SCGJX3KBU42EI';
-const pw = 'M36CFGm1CeW2LHIaNbdh0zyn7JXcqRsFOrLK7mr/';
-const bucket_name = 'lifeflavor';
+const AWS = require('aws-sdk');     //aws와 연동하는데 필요한 모듈을 가져옴
+const id = '';                //id key값 
+const pw = '';                //key값 password
+const bucket_name = '';      //bucket 이름
 const s3=new AWS.S3({
   accessKeyId: id,
   secretAccessKey: pw
 });
-const uploadFile = (fileName) => {
+const uploadFile = (fileName) => {                 //s3에 업로드하는 함수
   const fileContent = fs.readFileSync(fileName);
   const params = {
     Bucket: bucket_name,
@@ -36,4 +36,4 @@ const uploadFile = (fileName) => {
     console.log(`file uploaded successfully. ${data.Location}`);
   });
 };
-uploadFile('./recorded/dondo.mp4');
+uploadFile('./recorded/dondo.mp4');         /함수호출 경로
